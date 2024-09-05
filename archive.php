792c9ee3e9a4953245e4ffc2f1926746553b44f8
Arko <arkoroyb@gmail.com>
2024-09-05 11:01:24 +0600

    issue solve of infinite loop

--- archive.php
+++ archive.php
@@ -6,9 +6,11 @@ get_header();
     <div class="page-banner__bg-image"
         style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>)"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcome to our Blog</h1>
+        <h1 class="page-banner__title"><?php
+        the_archive_title();
+        ?></h1>
         <div class="page-banner__intro">
-            <p>Keep up with our latest news.</p>
+            <p><?php the_archive_description(); ?></p>
         </div>
     </div>
 
@@ -22,7 +24,8 @@ get_header();
             <h2 class="headline headline--medium headline--post-title"><a
                     href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
             <div class="metabox">
-                <p class="">Posted By <?php the_author_posts_link() ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+                <p class="">Posted By <?php the_author_posts_link() ?> on <?php the_time('n.j.y'); ?> in
+                    <?php echo get_the_category_list(', '); ?></p>
             </div>
             <div class="generic-content">
                 <?php the_excerpt(); ?>
